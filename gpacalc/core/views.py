350e49d5dbdec3cd40bbee7f3052a9bd84570f6f
Adam Beagle <abeagle88@gmail.com>
2014-05-02 17:20:39 -0700

    Add update view for grade scale.
    And corresponding urlconf, template, styles

--- gpacalc/core/views.py
+++ gpacalc/core/views.py
@@ -1,37 +1,37 @@
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import DeleteView, ListView
+from django.views.generic import DeleteView, FormView, ListView
 
-from .forms import SemesterForm, UClassFormset
-from .models import calculate_gpa, Semester, UClass
+from .forms import LetterGradeFormSet, SemesterForm, UClassFormSet
+from .models import calculate_gpa, LetterGrade, Semester, UClass
 
 def create_semester(request):
     # If POST, validate both forms, save, and redirect to index
     if request.method == 'POST': 
         semesterForm = SemesterForm(request.POST)
-        classesFormset = UClassFormset(request.POST)
+        classesFormSet = UClassFormSet(request.POST)
         
-        if semesterForm.is_valid() and classesFormset.is_valid():
+        if semesterForm.is_valid() and classesFormSet.is_valid():
             semesterInstance = semesterForm.save()
             
-            for form in classesFormset:
+            for form in classesFormSet:
                 form.instance.semester_id = semesterInstance.id
 
-            classesFormset.save()
+            classesFormSet.save()
             
             return HttpResponseRedirect(reverse_lazy('semester_index'))
     
     # If GET, prepare empty forms.
     else:
         semesterForm = SemesterForm()
-        classesFormset = UClassFormset(queryset=UClass.objects.none())
+        classesFormSet = UClassFormSet(queryset=UClass.objects.none())
         
     # Note if either form invalid in a POST, control jumps here and page
     # reloaded with forms containing error messages, etc.
     return render(request, 'core/semester_create.html', {
         'semester_form' : semesterForm,
-        'classes_formset' : classesFormset,
+        'classes_formset' : classesFormSet,
         'is_update' : False
     })
 
@@ -41,17 +41,17 @@ def update_semester(request, pk):
     # If POST, validate both forms, save, and redirect to index
     if request.method == 'POST': 
         semesterForm = SemesterForm(request.POST)
-        classesFormset = UClassFormset(request.POST)
-        classesFormset.can_delete = True 
+        classesFormSet = UClassFormSet(request.POST)
+        classesFormSet.can_delete = True 
         
-        if semesterForm.is_valid() and classesFormset.is_valid():
+        if semesterForm.is_valid() and classesFormSet.is_valid():
             semesterForm.instance.id = semester_id
             semesterForm.save()
             
-            for form in classesFormset:
+            for form in classesFormSet:
                 form.instance.semester_id = semester_id
 
-            classesFormset.save()
+            classesFormSet.save()
             
             return HttpResponseRedirect(reverse_lazy('semester_index'))
     
@@ -61,21 +61,45 @@ def update_semester(request, pk):
         semesterForm = SemesterForm(
             instance=Semester.objects.get(id=semester_id)
         )
-        classesFormset = UClassFormset(
+        classesFormSet = UClassFormSet(
             queryset=UClass.objects.filter(semester=semester_id),
         )
         
         # Class deletion allowed here; Semester deletion handled in 
         # SemesterDeleteView
-        classesFormset.can_delete = True 
+        classesFormSet.can_delete = True 
 
     # Note if either form invalid in a POST, control jumps here and page
     # reloaded with forms containing error messages, etc.
     return render(request, 'core/semester_create.html', {
         'semester_form' : semesterForm,
-        'classes_formset' : classesFormset,
+        'classes_formset' : classesFormSet,
         'is_update' : True
     })
+    
+class GradeScaleUpdateView(FormView):
+    model = LetterGrade
+    form_class = LetterGradeFormSet
+    template_name = 'core/gradescale_update.html'
+    success_url = reverse_lazy('semester_index')
+    
+    # TODO: Once users implemented, use user key to get correct scale.
+    queryset = LetterGrade.objects.filter(scale=1)
+
+    def post(self, request, *args, **kwargs):
+        formset = LetterGradeFormSet(request.POST)
+        for form in formset:
+            # TODO: Once users implemented, use user key to get correct
+            # scale.
+            form.instance.scale_id = 1
+        
+        if formset.is_valid():
+            formset.save()
+            
+            return HttpResponseRedirect(reverse_lazy('semester_index'))
+        
+        context = self.get_context_data(form=formset)
+        return render(request, self.template_name, context)
 
 class SemestersIndex(ListView):
     queryset = Semester.objects.select_related('uclass')
--- gpacalc/gpacalc/urls.py
+++ gpacalc/gpacalc/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from core.views import (create_semester, SemesterDeleteView, update_semester, 
-    SemestersIndex)
+from core.views import (create_semester, GradeScaleUpdateView, 
+    SemesterDeleteView, update_semester, SemestersIndex)
 
 
 admin.autodiscover()
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^$', SemestersIndex.as_view(), name='semester_index'),
     url(r'^create/semester/$', create_semester, name='create_semester'),
     url(r'^edit/semester/(?P<pk>\w+)/$', update_semester, name='update_semester'),
+    url(r'^edit/grade-scale/$', GradeScaleUpdateView.as_view(), name='update_gradescale'),
     url(r'^delete/semester/(?P<pk>\w+)/$', SemesterDeleteView.as_view(), name='delete_semester'),
     url(r'^admin/', include(admin.site.urls)),
 )
