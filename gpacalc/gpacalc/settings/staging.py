4ecc5a3de73897454162a4151800eb90165880dc
Adam Beagle <abeagle88@gmail.com>
2014-07-27 15:05:03 -0700

    Set DEBUG false for staging

--- gpacalc/gpacalc/settings/staging.py
+++ gpacalc/gpacalc/settings/staging.py
@@ -2,7 +2,7 @@ from .base import *
 
 import dj_database_url 
 DATABASES = { 'default': dj_database_url.config() }
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
